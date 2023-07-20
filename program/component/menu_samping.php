4ef3d7d61ca0d7e987bbc669ffe13da4a74ffb50
alippratama <alippratama04@gmail.com>
2023-07-20 15:08:36 +0700

    bg alif

--- program/component/menu_samping.php
+++ program/component/menu_samping.php
@@ -64,10 +64,18 @@
               <i class="bx bx-chevron-left bx-sm align-middle"></i>
             </a>
           </div>
+          <?php include "koneksi.php" ?>
+          <li style="margin-left: 30px;" class=" small text-uppercase"><span class="menu-header-text">welcome 
+          <?php echo $_SESSION['username']; ?>
+            
+          </span></li>
           <div class="menu-inner-shadow"></div>
           <ul class="menu-inner py-1">
             <!-- Dashboard -->
             <li class="menu-header small text-uppercase"><span class="menu-header-text">Dashboard</span></li>
+
+  
+
             <li class="menu-item active">
               <a href="index.php?page=jurusan" class="menu-link">
                 <div data-i18n="Analytics">Jurusan</div>
