da3d7903aaa77b27354a6e6e1591a3e3dd4edb36
Raja Hariadi <rajahariadi0@gmail.com>
2023-07-21 23:55:30 +0700

    data

--- program/database/program.sql
+++ program/database/program.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 19, 2023 at 07:55 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Waktu pembuatan: 21 Jul 2023 pada 18.52
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,43 +24,145 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_login`
+-- Struktur dari tabel `jurusan`
+--
+
+CREATE TABLE `jurusan` (
+  `kode` int(11) NOT NULL,
+  `nm_jurusan` varchar(100) DEFAULT NULL,
+  `jm_sks` int(11) DEFAULT NULL,
+  `jj` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `jurusan`
+--
+
+INSERT INTO `jurusan` (`kode`, `nm_jurusan`, `jm_sks`, `jj`) VALUES
+(8, 'Teknik Kimia', 120, 'S1');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_kelas`
+--
+
+CREATE TABLE `tb_kelas` (
+  `id_kelas` int(11) NOT NULL,
+  `kelas` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tb_kelas`
+--
+
+INSERT INTO `tb_kelas` (`id_kelas`, `kelas`) VALUES
+(1, 'Pagi A'),
+(5, 'sore'),
+(6, 'Pagi B');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_login`
 --
 
 CREATE TABLE `tb_login` (
-  `id` int(24) NOT NULL,
+  `id` int(11) NOT NULL,
   `username` varchar(12) NOT NULL,
   `password` varchar(8) NOT NULL,
   `level` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tb_login`
+-- Dumping data untuk tabel `tb_login`
 --
 
 INSERT INTO `tb_login` (`id`, `username`, `password`, `level`) VALUES
 (1, 'admin', 'Admin', 'admin'),
 (3, 'user', 'user', 'user');
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_mahasiswa`
+--
+
+CREATE TABLE `tb_mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nim` varchar(10) DEFAULT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `jenis_kelamin` varchar(10) DEFAULT NULL,
+  `no_telp` varchar(15) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `foto` blob DEFAULT NULL,
+  `kode` int(11) DEFAULT NULL,
+  `id_kelas` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tb_mahasiswa`
+--
+
+INSERT INTO `tb_mahasiswa` (`id`, `nim`, `nama`, `jenis_kelamin`, `no_telp`, `alamat`, `foto`, `kode`, `id_kelas`) VALUES
+(4, '2155201074', 'Raja Hariadi', 'lakilaki', '1234', 'JM', '', 8, NULL);
+
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tb_login`
+-- Indeks untuk tabel `jurusan`
 --
-ALTER TABLE `tb_login`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `jurusan`
+  ADD PRIMARY KEY (`kode`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indeks untuk tabel `tb_kelas`
+--
+ALTER TABLE `tb_kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indeks untuk tabel `tb_mahasiswa`
+--
+ALTER TABLE `tb_mahasiswa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kode` (`kode`),
+  ADD KEY `id_kelas` (`id_kelas`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `tb_login`
+-- AUTO_INCREMENT untuk tabel `jurusan`
 --
-ALTER TABLE `tb_login`
-  MODIFY `id` int(24) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `jurusan`
+  MODIFY `kode` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_kelas`
+--
+ALTER TABLE `tb_kelas`
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_mahasiswa`
+--
+ALTER TABLE `tb_mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tb_mahasiswa`
+--
+ALTER TABLE `tb_mahasiswa`
+  ADD CONSTRAINT `tb_mahasiswa_ibfk_1` FOREIGN KEY (`kode`) REFERENCES `jurusan` (`kode`),
+  ADD CONSTRAINT `tb_mahasiswa_ibfk_2` FOREIGN KEY (`id_kelas`) REFERENCES `tb_kelas` (`id_kelas`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
