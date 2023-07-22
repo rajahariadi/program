47bd9cac5c47189492324a0794df63a3fe4aa00f
Raja Hariadi <rajahariadi0@gmail.com>
2023-07-23 02:52:12 +0700

    utiwi

--- program/component/menu_samping.php
+++ program/component/menu_samping.php
@@ -1,52 +1,65 @@
-
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
-          <div class="app-brand demo">
-            <a href="index.php?page=welcome" class="app-brand-link">
-            <img src="assets/img/stt/logo.png" alt="" width="50px">              
-              <span style="font-size: 25px;"class=" demo menu-text fw-bolder ms-2" >STT Dumai</span>
-            </a>
+  <div class="app-brand demo">
+    <a href="index.php?page=welcome" class="app-brand-link">
+      <img src="assets/img/stt/logo.png" alt="" width="50px">
+      <span style="font-size: 25px;" class=" demo menu-text fw-bolder ms-2">STT Dumai</span>
+    </a>
 
-            <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
-              <i class="bx bx-chevron-left bx-sm align-middle"></i>
-            </a>
-          </div>
-          <?php include "koneksi.php" ?>
-          <li style="margin-left: 30px;" class=" small text-uppercase"><span class="menu-header-text">welcome 
-          <?php echo $_SESSION['username']; ?>
-            
-          </span></li>
-          <div class="menu-inner-shadow"></div>
-          <ul class="menu-inner py-1">
-            <!-- Dashboard -->
-            <li class="menu-header small text-uppercase"><span class="menu-header-text">Dashboard</span></li>
+    <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+      <i class="bx bx-chevron-left bx-sm align-middle"></i>
+    </a>
+  </div>
+  <?php include "koneksi.php" ?>
+  <li style="margin-left: 30px;" class=" small text-uppercase"><span class="menu-header-text">welcome
+      <?php
+      $nama = $_SESSION['username'];
+      $query = "SELECT nama FROM tb_login WHERE username = '$nama'";
+      $result = mysqli_query($koneksi, $query);
+      $data = mysqli_fetch_assoc($result);
+      echo $data['nama'];
+      ?>
 
-  
+    </span></li>
+  <div class="menu-inner-shadow"></div>
+  <ul class="menu-inner py-1">
+    <!-- Dashboard -->
+    <li class="menu-header small text-uppercase"><span class="menu-header-text">Dashboard</span></li>
 
-            <li class="menu-item active">
-              <a href="index.php?page=jurusan" class="menu-link">
-                <div data-i18n="Analytics">Jurusan</div>
-              </a>
-            </li>
 
-            <li class="menu-item active">
-              <a href="index.php?page=kelas" class="menu-link">
-                <div data-i18n="Analytics">Kelas</div>
-              </a>
-            </li>
 
-            <li class="menu-item active">
-              <a href="index.php?page=mahasiswa" class="menu-link">
-                <div data-i18n="Analytics">Data Mahasiswa</div>
-              </a>
-            </li>
-            <li class="menu-header small text-uppercase"><span class="menu-header-text">Settings</span></li>
-            
-          
-            <li class="menu-item active">
-              <a href="component/logout.php" class="menu-link">
-                <div data-i18n="Analytics">Logout</div>
-              </a>
-            </li>
-          </ul>
-      
-        </aside>
+    <li class="menu-item active">
+      <a href="index.php?page=jurusan" class="menu-link">
+        <div data-i18n="Analytics">Jurusan</div>
+      </a>
+    </li>
+
+    <li class="menu-item active">
+      <a href="index.php?page=kelas" class="menu-link">
+        <div data-i18n="Analytics">Kelas</div>
+      </a>
+    </li>
+
+    <li class="menu-item active">
+      <a href="index.php?page=mahasiswa" class="menu-link">
+        <div data-i18n="Analytics">Data Mahasiswa</div>
+      </a>
+    </li>
+    <li class="menu-header small text-uppercase"><span class="menu-header-text">Settings</span></li>
+
+    <?php
+    if ($_SESSION['level'] == 'admin') {
+      ?>
+      <li class="menu-item active">
+        <a href="index.php?page=datauser" class="menu-link">
+          <div data-i18n="Analytics">Data User</div>
+        </a>
+      </li>
+    <?php } ?>
+    <li class="menu-item active">
+      <a href="component/logout.php" class="menu-link">
+        <div data-i18n="Analytics">Logout</div>
+      </a>
+    </li>
+  </ul>
+
+</aside>
--- program/daftar.php
+++ program/daftar.php
@@ -1,185 +1,194 @@
 <?php
+session_start();
 include "component/koneksi.php";
+$pesannama = "";
+$pesanuser = "";
+$pesanpass = "";
+$pesan = "";
 
-$message = "";
+if (isset($_POST['daftar'])) {
+  $nama = $_POST['nama'];
+  $username = $_POST['username'];
+  $password = $_POST['password'];
 
-if(isset($_POST['daftar'])){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $level = $_POST['level'];
+  if (empty($nama) && empty($username) && empty($password)) {
+    $pesan = "Nama, Username, dan password tidak boleh kosong";
+  } elseif (empty($nama) && empty($username)) {
+    $pesannama = "Nama harus diisi";
+    $pesanuser = "Username harus diisi";
+  } elseif (empty($nama) && empty($password)) {
+    $pesannama = "Nama harus diisi";
+    $pesanpass = "Password harus diisi";
+  } elseif (empty($username) && empty($password)) {
+    $pesanuser = "Username harus diisi";
+    $pesanpass = "Password harus diisi";
+  } elseif (empty($nama)) {
+    $pesannama = "Nama harus diisi";
+  } elseif (empty($username)) {
+    $pesanuser = "Username harus diisi";
+  } elseif (empty($password)) {
+    $pesanpass = "Password harus diisi";
+  } else {
+    // Periksa apakah username sudah ada di database
+    $check_query = "SELECT * FROM tb_login WHERE username = '$username'";
+    $check_result = mysqli_query($koneksi, $check_query);
 
-    // Validasi agar username dan password tidak kosong
-    if(empty($username) or empty($password) or empty($level) ){
-        $message = "Username dan password harus diisi.";
+    if (mysqli_num_rows($check_result) > 0) {
+      $pesan = "Username sudah ada. Silakan pilih username lain.";
     } else {
-        // Periksa apakah username sudah ada di database
-        $check_query = "SELECT * FROM tb_login WHERE username = '$username'";
-        $check_result = mysqli_query($koneksi, $check_query);
-
-        if(mysqli_num_rows($check_result) > 0){
-            $message = "Username sudah ada. Silakan pilih username lain.";
-        } else {
-            // Jika username belum ada, lakukan pendaftaran
-            $data = "INSERT INTO tb_login (username, password, level) VALUES ('$username', '$password','$level')";
-            $hasil = mysqli_query($koneksi, $data);
-            if($hasil){
-                header("location:login.php");
-            } else {
-                echo "Pendaftaran gagal.";
-            }
-        }
+      // Jika username belum ada, lakukan pendaftaran
+      $data = "INSERT INTO tb_login (nama, username, password, level) VALUES ('$nama', '$username', '$password','user')";
+      $hasil = mysqli_query($koneksi, $data);
+      if ($hasil) {
+        header("location:login.php");
+      } else {
+        echo "Pendaftaran gagal.";
+      }
     }
+  }
 }
+
 ?>
 
 <!DOCTYPE html>
-<html
-  lang="en"
-  class="light-style customizer-hide"
-  dir="ltr"
-  data-theme="theme-default"
-  data-assets-path="assets/"
-  data-template="vertical-menu-template-free"
->
-  <head>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
-    />
+<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="assets/"
+  data-template="vertical-menu-template-free">
 
-    <title>Login Basic web sekolah tinggi teknologi dumai</title>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport"
+    content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <meta name="description" content="" />
+  <title>Login Basic web sekolah tinggi teknologi dumai</title>
 
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.ico" />
+  <meta name="description" content="" />
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet"
-    />
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.ico" />
 
-    <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="assets/vendor/fonts/boxicons.css" />
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+    rel="stylesheet" />
 
-    <!-- Core CSS -->
-    <link rel="stylesheet" href="assets/vendor/css/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="assets/css/demo.css" />
+  <!-- Icons. Uncomment required icon fonts -->
+  <link rel="stylesheet" href="assets/vendor/fonts/boxicons.css" />
 
-    <!-- Vendors CSS -->
-    <link rel="stylesheet" href="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+  <!-- Core CSS -->
+  <link rel="stylesheet" href="assets/vendor/css/core.css" class="template-customizer-core-css" />
+  <link rel="stylesheet" href="assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+  <link rel="stylesheet" href="assets/css/demo.css" />
 
-    <!-- Page CSS -->
-    <!-- Page -->
-    <link rel="stylesheet" href="assets/vendor/css/pages/page-auth.css" />
-    <!-- Helpers -->
-    <script src="assets/vendor/js/helpers.js"></script>
+  <!-- Vendors CSS -->
+  <link rel="stylesheet" href="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="assets/js/config.js"></script>
-  </head>
+  <!-- Page CSS -->
+  <!-- Page -->
+  <link rel="stylesheet" href="assets/vendor/css/pages/page-auth.css" />
+  <!-- Helpers -->
+  <script src="assets/vendor/js/helpers.js"></script>
 
-  <body>
-    <!-- Content -->
+  <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+  <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+  <script src="assets/js/config.js"></script>
+</head>
 
-    <div class="container-xxl">
-      <div class="authentication-wrapper authentication-basic container-p-y">
-        <div class="authentication-inner">
-          <!-- Register -->
-          <div class="card">
-            <div class="card-body">
-              <!-- Logo -->
-              <div class="app-brand justify-content-center">
-              <img src="assets/img/stt/logo.png"  width=150px>
-                <a href="index.php" class="app-brand-link gap-2">
-                </a>
-              </div>
-              <!-- /Logo -->
-              <p class="mb-4 text-center">Sekolah Tinggi Teknologi Dumai</p>
+<body>
+  <!-- Content -->
 
-              <form id="formAuthentication" class="mb-3" action="" method="POST">
-                <div class="mb-3">
-                  <label for="email" class="form-label">Email or Username</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="email"
-                    name="username"
-                    placeholder="Enter your email or username"
-                    autofocus
-                  />
-                </div>
-                <div class="mb-3 form-password-toggle">
-                  <div class="d-flex justify-content-between">
-                    <label class="form-label" for="password">Password</label>
-                  </div>
-                  <div class="input-group input-group-merge">
-                    <input
-                      type="password"
-                      id="password"
-                      class="form-control"
-                      name="password"
-                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                      aria-describedby="password"
-                    />
-                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-                  </div>
-                </div>
-                <label for="email" class="form-label">Level User</label>
-
-                    <select name="level" id="" class="form-control">
-                        <option value="User">User</option>
-                        <option value="Admin">Admin</option>
-                    </select>
-                  
-                <div class="mb-3">  
-                </div>
-                <div class="mb-3">
-                  <button class="btn btn-primary d-grid w-100" name="daftar" type="submit">Sign in</button>
-                  <p class="text-center" style="color: red;"><?php echo $message; ?></p>
-                </div>
-              </form>
-              <p class="text-center">
-                <a href="login.php">
-                <span>Sudah Punya Akun</span>
-                </a>
-              </p>
-
-              
+  <div class="container-xxl">
+    <div class="authentication-wrapper authentication-basic container-p-y">
+      <div class="authentication-inner">
+        <!-- Register -->
+        <div class="card">
+          <div class="card-body">
+            <!-- Logo -->
+            <div class="app-brand justify-content-center">
+              <img src="assets/img/stt/logo.png" width=150px>
+              <a href="index.php" class="app-brand-link gap-2">
+              </a>
             </div>
+            <!-- /Logo -->
+            <p class="mb-4 text-center">Sekolah Tinggi Teknologi Dumai</p>
+
+            <form id="formAuthentication" class="mb-3" action="" method="POST">
+              <div class="mb-3">
+                <label for="email" class="form-label">Nama</label>
+                <input type="text" class="form-control" id="email" name="nama" placeholder="Masukkan nama anda"
+                  autofocus />
+                <p class="text-danger">
+                  <?php echo $pesannama; ?>
+                </p>
+              </div>
+              <div class="mb-3">
+                <label for="email" class="form-label">Username</label>
+                <input type="text" class="form-control" id="email" name="username" placeholder="Masukkan username anda"
+                  autofocus />
+                <p class="text-danger">
+                  <?php echo $pesanuser; ?>
+                </p>
+              </div>
+              <div class="mb-3 form-password-toggle">
+                <div class="d-flex justify-content-between">
+                  <label class="form-label" for="password">Password</label>
+                </div>
+                <div class="input-group input-group-merge">
+                  <input type="password" id="password" class="form-control" name="password"
+                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                    aria-describedby="password" />
+                  <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                </div>
+                <p class="text-danger">
+                  <?php echo $pesanpass; ?>
+                </p>
+              </div>
+              <div class="mb-3">
+              </div>
+              <div class="mb-3">
+                <button class="btn btn-primary d-grid w-100" name="daftar" type="submit">Daftar</button>
+                <p class="text-center" style="color: red;">
+                  <?php echo $pesan; ?>
+                </p>
+              </div>
+            </form>
+            <p class="text-center">
+              <a href="login.php">
+                <span>Sudah Punya Akun</span>
+              </a>
+            </p>
+
+
           </div>
-          <!-- /Register -->
         </div>
+        <!-- /Register -->
       </div>
     </div>
+  </div>
 
-    <!-- / Content -->
+  <!-- / Content -->
 
-    
-    <!-- Core JS -->
-    <!-- build:js assets/vendor/js/core.js -->
-    <script src="assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="assets/vendor/libs/popper/popper.js"></script>
-    <script src="assets/vendor/js/bootstrap.js"></script>
-    <script src="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
 
-    <script src="assets/vendor/js/menu.js"></script>
-    <!-- endbuild -->
+  <!-- Core JS -->
+  <!-- build:js assets/vendor/js/core.js -->
+  <script src="assets/vendor/libs/jquery/jquery.js"></script>
+  <script src="assets/vendor/libs/popper/popper.js"></script>
+  <script src="assets/vendor/js/bootstrap.js"></script>
+  <script src="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
 
-    <!-- Vendors JS -->
+  <script src="assets/vendor/js/menu.js"></script>
+  <!-- endbuild -->
 
-    <!-- Main JS -->
-    <script src="assets/js/main.js"></script>
+  <!-- Vendors JS -->
 
-    <!-- Page JS -->
+  <!-- Main JS -->
+  <script src="assets/js/main.js"></script>
 
-    <!-- Place this tag in your head or just before your close body tag. -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-  </body>
+  <!-- Page JS -->
+
+  <!-- Place this tag in your head or just before your close body tag. -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+</body>
+
 </html>
