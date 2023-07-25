213efa2fa6737a19c77c40041a6ead47da3ec3bd
alippratama <alippratama04@gmail.com>
2023-07-26 00:15:54 +0700

    12:15 7/26

--- program/index.php
+++ program/index.php
@@ -37,17 +37,13 @@ if(!isset($_SESSION['level'])){
               </a>
             </div>
 
-            <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
-              <!-- Search -->
+            <div class="navbar-nav-center d-flex align-items-center" id="navbar-collapse">
+            <label class="navbar-label" for="">Aplikasi AcademiX</label>  
+            <!-- Search -->
               <div class="navbar-nav align-items-center">
                 <div class="nav-item d-flex align-items-center">
-                  <i class="bx bx-search fs-4 lh-0"></i>
-                  <input
-                    type="text"
-                    class="form-control border-0 shadow-none"
-                    placeholder="Search..."
-                    aria-label="Search..."
-                  />
+                  
+                  
                 </div>
               </div>
               <!-- /Search -->
