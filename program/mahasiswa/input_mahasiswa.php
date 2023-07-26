a154f2a0ea5f2a7db96821ed0de78255c066f32e
Raja Hariadi <rajahariadi0@gmail.com>
2023-07-27 05:10:03 +0700

    punya septi

--- program/mahasiswa/input_mahasiswa.php
+++ program/mahasiswa/input_mahasiswa.php
@@ -1,113 +1,62 @@
 <?php
-// include "proses/koneksi.php";
-// $pesannim = "";
-// $pesannama = "";
-// $pesanjurusan = "";
-// $pesankelas = "";
-// $pesangender = "";
-// $pesannotelp = "";
-// $pesanalamat = "";
-// $pesanfoto = "";
-// $pesan = "";
+include "proses/koneksi.php";
 
-// if (isset($_POST['Simpan'])) {
-// $nim = $_POST['nim'];
-// $nama = $_POST['nama'];
-// $jurusan = $_POST['jurusan'];
-// $kelas = $_POST['kelas'];
-// $gender = $_POST['jeniskelamin'];
-// $notelp = $_POST['notelp'];
-// $alamat = $_POST['alamat'];
-// $foto = $_POST['foto'];
+$pesannim = "";
+$pesannama = "";
+$pesanjurusan = "";
+$pesankelas = "";
+$pesangender = "";
+$pesannotelp = "";
+$pesanalamat = "";
+$pesanfoto = "";
+$pesan = "";
 
-//   if (empty($nim) && empty($nama) && empty($jurusan) && empty($kelas) && empty($gender) && empty($notelp) && empty($alamat) && empty($foto)) {
-//     $pesannim = "Nim harus diisi";
-//     $pesannama = "Nama harus diisi";
-//     $pesanjurusan = "Jurusan harus diisi";
-//     $pesankelas = "Kelas harus diisi";
-//     $pesangender = "Gender harus diisi";
-//     $pesannotelp = "No Telp harus diisi";
-//     $pesanalamat = "Alamat harus diisi";
-//     $pesanfoto = "Foto harus diisi";
-//   } elseif  (empty($nim) && empty($nama) && empty($jurusan)){
-//     $pesannim = "Nim harus diisi";
-//     $pesannama = "Nama harus diisi";
-//     $pesanjurusan = "Jurusan harus diisi";
-//   } elseif (empty($nama) && empty($jurusan) && empty($kelas)) {
-//     $pesannama = "Nama harus diisi";
-//     $pesanjurusan = "Jurusan harus diisi";
-//     $pesankelas = "Kelas harus diisi";
-//   } elseif (empty($jurusan) && empty($kelas) && empty($gender)) {
-//     $pesanjurusan = "Jurusan harus diisi";
-//     $pesankelas = "Kelas harus diisi";
-//     $pesangender = "Gender harus diisi";
-//   } elseif (empty($kelas) && empty($gender) && empty($notelp)) {
-//     $pesankelas = "Kelas harus diisi";
-//     $pesangender = "Gender harus diisi";
-//     $pesannotelp = "No Telp harus diisi";
-//   } elseif (empty($gender) && empty($notelp) && empty($alamat)) {
-//     $pesangender = "Gender harus diisi";
-//     $pesannotelp = "No Telp harus diisi";
-//     $pesanalamat = "Alamat harus diisi";
-//   } elseif (empty($notelp) && empty($alamat) && empty($foto)) {
-//     $pesannotelp = "No Telp harus diisi";
-//     $pesanalamat = "Alamat harus diisi";
-//     $pesanfoto = "Foto harus diisi";
-//   } elseif (empty($nim) && empty($nama)) {
-//     $pesannim = "Nim harus diisi";
-//     $pesannama = "Nama harus diisi";
-//   } elseif (empty($nama) && empty($jurusan)) {
-//     $pesannama = "Nama harus diisi";
-//     $pesanjurusan = "Jurusan harus diisi";
-//   } elseif (empty($jurusan) && empty($kelas)) {
-//     $pesanjurusan = "Jurusan harus diisi";
-//     $pesankelas = "Kelas harus diisi";
-//   } elseif (empty($kelas) && empty($gender)) {
-//     $pesankelas = "Kelas harus diisi";
-//     $pesangender = "Gender harus diisi";
-//   } elseif (empty($gender) && empty($notelp)) {
-//     $pesangender = "Gender harus diisi";
-//     $pesannotelp = "No Telp harus diisi";
-//   } elseif (empty($notelp) && empty($alamat)) {
-//     $pesannotelp = "No Telp harus diisi";
-//     $pesanalamat = "Alamat harus diisi";
-//   } elseif (empty($alamat) && empty($foto)) {
-//     $pesanalamat = "Alamat harus diisi";
-//     $pesanfoto = "Foto harus diisi";
-//   } elseif (empty($nim)) {
-//     $pesanjurusan = "Jurusan harus diisi";
-//     $pesankelas = "Kelas harus diisi";
-//   } elseif (empty($kelas) && empty($gender)) {
-//     $pesankelas = "Kelas harus diisi";
-//     $pesangender = "Gender harus diisi";
-//   } elseif (empty($gender) && empty($notelp)) {
-//     $pesangender = "Gender harus diisi";
-//     $pesannotelp = "No Telp harus diisi";
-//   } elseif (empty($notelp) && empty($alamat)) {
-//     $pesannotelp = "No Telp harus diisi";
-//     $pesanalamat = "Alamat harus diisi";
-//   } elseif (empty($alamat) && empty($foto)) {
-//     $pesanalamat = "Alamat harus diisi";
-//     $pesanfoto = "Foto harus diisi";
-//   } else {
-//       // Periksa apakah username dan level sudah ada di database
-//       $check_query = "SELECT * FROM tb_mahasiswa WHERE nim = '$nim'";
-//       $check_result = mysqli_query($koneksi, $check_query);
+if (isset($_POST['Simpan'])) {
+  $nim = $_POST['nim'];
+  $nama = $_POST['nama'];
+  $jurusan = $_POST['jurusan'];
+  $kelas = $_POST['kelas'];
+  $jeniskelamin = isset($_POST['jeniskelamin']) ? $_POST['jeniskelamin'] : '';
+  $notelp = $_POST['notelp'];
+  $alamat = $_POST['alamat'];
+  if (empty($nim)) {
+    $pesannim = "Nim harus diisi";
+  }
+  if (empty($nama)) {
+    $pesannama = "Nama harus diisi";
+  }
+  if (empty($jurusan)) {
+    $pesanjurusan = "Jurusan harus diisi";
+  }
+  if (empty($kelas)) {
+    $pesankelas = "Kelas harus diisi";
+  }
+  if (empty($gender)) {
+    $pesangender = "Gender harus diisi";
+  }
+  if (empty($notelp)) {
+    $pesannotelp = "No Telp harus diisi";
+  }
+  if (empty($alamat)) {
+    $pesanalamat = "Alamat harus diisi";
+  }
 
-//       if (mysqli_num_rows($check_result) > 0) {
-//           $pesan = "Nim sudah ada. Silakan masukkan Nim lain.";
-//       } else {
-//           // Jika username belum ada, lakukan pendaftaran
-//           $data = "INSERT INTO tb_mahasiswa (nim, nama, jenis_kelamin, no_telp, alamat, foto, kode, id_kelas) VALUES ('$nim','$nama','$jeniskelamin','$notelp', '$alamat','$namafile','$jurusan','$kelas')";
-//           $hasil = mysqli_query($koneksi, $data);
-//           echo '<script>window.location.href = "index.php?page=mahasiswa";</script>';
-//               // header("location: ../index.php?page=datauser");
-//       }
-//   }
-// }
+   else {
+    $namafile = basename($_FILES['foto']['name']);
+    $target_file = "mahasiswa/foto/" . basename($_FILES["foto"]["name"]);
+
+    $upload = move_uploaded_file($_FILES["foto"]["tmp_name"], $target_file);
+
+    $q = "INSERT INTO tb_mahasiswa (nim, nama, jenis_kelamin, no_telp, alamat, foto, kode, id_kelas) VALUES ('$nim','$nama','$jeniskelamin','$notelp', '$alamat','$namafile','$jurusan','$kelas')";
+
+
+    mysqli_query($koneksi, $q);
+
+    echo '<script>window.location.href = "index.php?page=mahasiswa";</script>';
+}
+}
+
 ?>
-
-
 
 <div class="card card-primary">
   <div class="card-header">
@@ -115,18 +64,21 @@
       <i class="fa fa-edit"></i> Tambah Data Mahasiswa
     </h3>
   </div>
-  <form action="mahasiswa/ex_mahasiswa.php" method="post" enctype="multipart/form-data">
+  <form action="" method="post" enctype="multipart/form-data">
     <div class="card-body">
-    <div class="form-group row">
+      <div class="form-group row">
         <div class="col-sm-6">
-          <input type="hidden" class="form-control" id="" name="id" placeholder="Nim" required>
+          <input type="hidden" class="form-control" id="" name="id" placeholder="Nim" d>
         </div>
       </div>
 
       <div class="form-group row">
         <label class="col-sm-2 col-form-label">Nim</label>
         <div class="col-sm-6">
-          <input type="text" class="form-control" id="" name="nim" placeholder="Nim" required>
+          <input type="text" class="form-control" id="" name="nim" placeholder="Nim" >
+          <p class="col-form-label" style="color: red;">
+                        <?php echo $pesannim ?>
+                    </p>
         </div>
       </div>
       <br>
@@ -134,7 +86,10 @@
       <div class="form-group row">
         <label class="col-sm-2 col-form-label">Nama Mahasiswa</label>
         <div class="col-sm-6">
-          <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Mahasiswa" required>
+          <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Mahasiswa" >
+          <p class="col-form-label" style="color: red;">
+                        <?php echo $pesannama ?>
+                    </p>
         </div>
       </div>
       <br>
@@ -142,21 +97,24 @@
       <div class="form-group row">
         <label class="col-sm-2 col-form-label">Jurusan</label>
         <div class="col-sm-4">
-          <select name="jurusan" id="jurusan" class="form-control" required>
-            <option>- Pilih -</option>
-            <?php 
+          <select name="jurusan" id="jurusan" class="form-control" >
+            <option value="">- Pilih -</option>
+            <?php
             //ambil data jurusan
             $query = "SELECT * FROM jurusan";
             $hasil = mysqli_query($koneksi, $query);
-            while ($row = mysqli_fetch_array($hasil)){
+            while ($row = mysqli_fetch_array($hasil)) {
               ?>
-              <option value = "<?= $row['kode']?>">
-              <?= $row['nm_jurusan'] ?>
+              <option value="<?= $row['kode'] ?>">
+                <?= $row['nm_jurusan'] ?>
               </option>
-            <?php
+              <?php
             }
             ?>
           </select>
+          <p class="col-form-label" style="color: red;">
+                        <?php echo $pesanjurusan ?>
+                    </p>
         </div>
       </div>
       <br>
@@ -164,13 +122,13 @@
       <div class="form-group row">
         <label class="col-sm-2 col-form-label">Kelas</label>
         <div class="col-sm-4">
-          <select name="kelas" id="kelas" class="form-control" required>
-            <option>- Pilih -</option>
+          <select name="kelas" id="kelas" class="form-control" >
+            <option value="">- Pilih -</option>
             <?php
             //ambil data dari database
             $query = "SELECT * FROM tb_kelas";
             $hasil = mysqli_query($koneksi, $query);
-            while ($row = mysqli_fetch_array($hasil)){
+            while ($row = mysqli_fetch_array($hasil)) {
               ?>
               <option value="<?php echo $row['id_kelas'] ?>">
                 <?php echo $row['kelas'] ?>
@@ -179,6 +137,9 @@
             }
             ?>
           </select>
+          <p class="col-form-label" style="color: red;">
+                        <?php echo $pesankelas ?>
+                    </p>
         </div>
       </div>
       <br>
@@ -199,15 +160,21 @@
       <div class="form-group row">
         <label class="col-sm-2 col-form-label">No Telp</label>
         <div class="col-sm-6">
-          <input type="text" class="form-control" id="notelp" name="notelp" placeholder="No Telp" required>
+          <input type="text" class="form-control" id="notelp" name="notelp" placeholder="No Telp" >
+          <p class="col-form-label" style="color: red;">
+                        <?php echo $pesannotelp ?>
+                    </p>
         </div>
       </div>
       <br>
-      
+
       <div class="form-group row">
         <label class="col-sm-2 col-form-label">Alamat</label>
         <div class="col-sm-6">
-          <input type="text" class="form-control" id="alamat" name="alamat" placeholder="Alamat" required>
+          <input type="text" class="form-control" id="alamat" name="alamat" placeholder="Alamat" >
+          <p class="col-form-label" style="color: red;">
+                        <?php echo $pesanalamat ?>
+                    </p>
         </div>
       </div>
       <br>
@@ -215,7 +182,8 @@
       <div class="form-group row">
         <label class="col-sm-2 col-form-label">Foto</label>
         <div class="col-sm-6">
-          <input type="file" class="form-control" id="foto" name="foto" placeholder="" required>
+          <input type="file" class="form-control" id="foto" name="foto" placeholder="" >
+          
         </div>
       </div>
       <br>
